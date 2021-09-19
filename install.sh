95199340d6ac6af7bae352ec3126e43437a2d933
Stonzyy <33039121+Stonzyy@users.noreply.github.com>
2021-09-20 02:09:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install golang;go get github.com/tomnomnom/waybackurls;GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder;go get github.com/Emoe/kxss
+sudo apt-get install golang;go get github.com/tomnomnom/waybackurls;GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder;go get github.com/Emoe/kxss;GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
 
 cd ~/go/bin; sudo cp * /usr/bin/
 
